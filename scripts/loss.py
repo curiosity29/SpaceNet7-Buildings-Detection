e021d5470e7090d56020ae5c5a2858677141ad48
curiosity29 <62107278+curiosity29@users.noreply.github.com>
2023-12-04 08:32:40 +0700

    Update loss.py

--- scripts/loss.py
+++ scripts/loss.py
@@ -8,11 +8,12 @@ class LossSpaceNet7(nn.Module):
         super().__init__()
         # self.dice = DiceLoss(sigmoid=True, batch=True)
         # self.ce = FocalLoss(gamma=2.0, to_onehot_y=False)
+        self.ce = CrossEntropyLoss(reduction = 'mean')
         
     def _loss(self, p, y):
         # return self.dice(p, y) + self.ce(p, y)
 
-        return CrossEntropyLoss(p,y)
+        return self.ce(p,y)
     
     def forward(self, p, y):
         return self._loss(p, y)
