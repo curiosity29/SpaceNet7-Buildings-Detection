d28663c9b394d362c3a9845b9749d24d5ee941c0
curiosity29 <62107278+curiosity29@users.noreply.github.com>
2023-12-04 11:28:05 +0700

    Update loss.py

--- scripts/loss.py
+++ scripts/loss.py
@@ -1,23 +1,27 @@
 import torch.nn as nn
 from monai.losses import DiceLoss, FocalLoss
-from monai.losses.dice import *
-from monai.networks import one_hot
+# from monai.losses.dice import *
+# from monai.networks import one_hot
 from torch.nn import CrossEntropyLoss
+
+from torchmetrics import Dice
 
 
 class LossSpaceNet7(nn.Module):
     def __init__(self, n_class):
         super().__init__()
         self.n_class = n_class
-        self.dice = DiceLoss(sigmoid=True, batch=True)
+        # self.dice = DiceLoss(sigmoid=True, batch=True)
         # self.ce = FocalLoss(gamma=2.0, to_onehot_y=False)
-        self.ce = CrossEntropyLoss(reduction = 'sum')
+
+        self.dice = Dice(num_classes = self.n_class, ignore_index = 1)
+        self.ce = CrossEntropyLoss(reduction = 'average')
         
     def _loss(self, p, y):
         # one hot n_class
-        one_hot_y = one_hot(y[:, None, ...], num_classes = self.n_class)
+        # one_hot_y = one_hot(y[:, None, ...], num_classes = self.n_class)
 
-        dice_loss = self.dice(p, one_hot_y)
+        dice_loss = self.dice(p, y)
         categorical_loss = self.ce(p, y)
 
         return dice_loss + categorical_loss
