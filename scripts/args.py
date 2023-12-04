a74113e1dc68cdc144de52a6545694541273f03b
curiosity29 <62107278+curiosity29@users.noreply.github.com>
2023-12-04 10:21:27 +0700

    Update args.py

--- scripts/args.py
+++ scripts/args.py
@@ -27,7 +27,7 @@ def get_main_args():
 
     arg ("--num_workers", type=int, default=2, help="DataLoader num_workers")
     arg ("--in_channels", type=int, default=4, help="#Input Channels")
-    arg ("--out_channels", type=int, default=9, help="#Output Channels")
+    arg ("--out_channels", type=int, default=8, help="#Output Channels")
     arg ("--learning_rate", type=float, default=1e-4, help="Learning Rate")
     arg ("--weigh_decay", type=float, default=1e-5, help="Weigh Decay")
     arg ("--kernels",  default=[[3, 3]] * 5, help="Convolution Kernels")
